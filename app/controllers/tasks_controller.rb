f9e9a4a884f2faaee2ff65f884361f04ebba5ad7
dai.tana <0108.sdkill@gmail.com>
2020-02-14 17:06:04 +0000

    CRUD tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,55 @@
+class TasksController < ApplicationController
+  before_action :task_set, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @tasks = Task.all
+  end
+  
+  def show
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:success] = "タスクは正常に登録されました。"
+      redirect_to @task
+    else
+      flash.now[:danger] = "タスクは登録されませんでした。"
+      render :new
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @task.update(task_params)
+      flash[:success] = "タスクは正常に更新されました。"
+      redirect_to @task
+    else
+      flash.now[:danger] = "タスクの更新はできませんでした。"
+      render :edit
+    end
+  end
+  
+  def destroy
+    @task.destroy
+    flash[:success] = "タスクは削除されました。"
+    redirect_to tasks_url
+  end
+  
+  private
+  
+  def task_params
+    params.require(:task).permit(:content)
+  end
+  
+  def task_set
+    @task = Task.find(params[:id])
+  end
+end
