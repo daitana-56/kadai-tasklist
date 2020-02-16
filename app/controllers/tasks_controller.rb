bae32b69d3172989ca6d1b273bea1b177651e88e
dai.tana <0108.sdkill@gmail.com>
2020-02-16 07:45:46 +0000

    review amendment

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
-  before_action :task_set, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
+  before_action :task_set, only: [:show, :edit, :update, :destroy]
   
   def index
     @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(20)
@@ -51,6 +51,10 @@ class TasksController < ApplicationController
   end
   
   def task_set
-    @task = current_user.tasks.find(params[:id])
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
+
 end
