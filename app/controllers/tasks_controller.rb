1bc72e1ab04fd009ac208541b9016ed171eee27d
dai.tana <0108.sdkill@gmail.com>
2020-02-16 02:44:44 +0000

    user login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :task_set, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
   
   def index
     @tasks = Task.order(id: :desc).page(params[:page]).per(20)
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,9 @@ Rails.application.routes.draw do
   #user用ルーティング
   get 'signup', to: 'users#new'
   resources :users, only: [:new, :create]
+  
+  #session用ルーティング
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
